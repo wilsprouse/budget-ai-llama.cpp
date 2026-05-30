3c90904d404ffc96298e2287eecdf9df251c77c3
wsprouse <wilsprouse@gmail.com>
2026-05-29 21:13:58 -0400

    Use correct model

--- api.py
+++ api.py
@@ -38,7 +38,7 @@ class Message(BaseModel):
     content: str
 
 class GenerateRequest(BaseModel):
-    model: Optional[str] = "TheBloke/Mistral-7B-Instruct-v0.3-AWQ"
+    model: Optional[str] = MODEL_NAME
     messages: List[Message]
     max_tokens: Optional[int] = 128
     temperature: Optional[float] = 0.7
